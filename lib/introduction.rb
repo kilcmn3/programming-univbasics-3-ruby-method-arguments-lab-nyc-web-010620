117127fd5a48f87cb6b2d564ce3e6106667416cd
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-26 02:03:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -16,7 +16,7 @@ end
 
  introduction_with_language("David", "ruby")
 
- def introduction_with_language_optional(name, language = "ruby")
+ def introduction_with_language_optional(name, language = "Ruby")
    puts "Hi, my name is #{name} and I am learning to program in #{language}."\
  end
 
